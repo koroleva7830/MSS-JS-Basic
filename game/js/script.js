04132e05f0e1b71a5c990c66e12ec530be8f5a2f
koroleva7830 <31980679+koroleva7830@users.noreply.github.com>
2018-11-05 16:16:33 +0300

    задание 3.4

--- game/js/script.js
+++ game/js/script.js
@@ -1,1 +1,7 @@
 console.log('You are at ' + window.location);
+let money = prompt('money?');
+let apple = prompt('apple?');
+let bread = prompt('bread?');
+let sumapple=prompt('price of apple?')*apple;
+let sumbread=prompt('price of bread?')*bread;
+document.body.innerHTML = money > (sumbread + sumapple);
